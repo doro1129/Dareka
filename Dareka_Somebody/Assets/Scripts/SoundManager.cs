62f0259d774bd4dc791a28bf6cc9d4d1f928a259
doro1129 <doro1129@naver.com>
2022-10-15 18:45:58 +0900

    Add UI Sound Effect

--- Dareka_Somebody/Assets/Scripts/SoundManager.cs
+++ Dareka_Somebody/Assets/Scripts/SoundManager.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource bgSound;
+    public AudioClip[] bglist;
+
     public static SoundManager instance;
 
     private void Awake()
@@ -12,10 +17,23 @@ public class SoundManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(instance);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for (int i = 0; i < bglist.Length; i++)
+        {
+            if(arg0.name == bglist[i].name)
+            {
+                BgSoundPlay(bglist[i]);
+                break;
+            }
         }
     }
 
@@ -27,5 +45,13 @@ public class SoundManager : MonoBehaviour
         audioSource.Play();
 
         Destroy(sound, clip.length);
+    }
+
+    public void BgSoundPlay(AudioClip clip)
+    {
+        bgSound.clip = clip;
+        bgSound.loop = true;
+        bgSound.volume = 0.1f;
+        bgSound.Play();
     }
 }
