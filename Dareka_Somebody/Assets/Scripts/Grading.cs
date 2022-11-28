135742b58370149588f2f6f8db5060b3b96355b5
JunHyun <thesimpson33@gmail.com>
2022-11-28 23:54:00 +0900

    Add grading script

---
+++ Dareka_Somebody/Assets/Scripts/Grading.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Grading : MonoBehaviour
+{
+    public List<InputField> Answer = new List<InputField>(new InputField[4]);
+
+    private List<string> Answer_string = new List<string>(new string[4]);
+    private int score = 0;
+    private string[] Answers = new string[4] { "1", "2", "3", "4" };
+
+    private void Grade()
+    {
+        SetAnswer();
+
+        for (int i = 0; i <= 3; i++)
+        {
+            if (Answer_string[i] == Answers[i])
+            {
+                score += 25;
+            }
+        }
+        Debug.Log(score);
+    }
+
+    private void SetAnswer()
+    {
+        for(int i = 0; i <= 3; i++)
+        {
+            Answer_string[i] = Answer[i].GetComponent<InputField>().text;
+        }
+    }
+}
