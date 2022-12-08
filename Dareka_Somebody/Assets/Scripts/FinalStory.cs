6a2e40053f9b427c1021c44e70a6fb103b97e2a7
Junhyun <thesimpson33@gmail.com>
2022-12-08 13:19:00 +0900

    Change final dialogue

--- Dareka_Somebody/Assets/Scripts/FinalStory.cs
+++ Dareka_Somebody/Assets/Scripts/FinalStory.cs
@@ -26,7 +26,7 @@ public class FinalStory : MonoBehaviour
             Talking();
         }
 
-        if (Finaldialogue_count == 4)
+        if (Finaldialogue_count == 6)
         {
             //sceneControl.OnclickStageScene();
             SceneManager.LoadScene(5);
@@ -36,12 +36,12 @@ public class FinalStory : MonoBehaviour
 
     public void Talking()
     {
-        if (Finaldialogue_count <= 2)
+        if (Finaldialogue_count <= 4)
         {
             FinalDialogue_Text.text = FinalDialogue_String[Finaldialogue_count];
             Finaldialogue_count++;
         }
-        else if (Finaldialogue_count == 3)
+        else if (Finaldialogue_count == 5)
         {
             FinalDialogue_Text.text = FinalDialogue_String[Finaldialogue_count - 1];
             Finaldialogue_count++;
@@ -50,11 +50,13 @@ public class FinalStory : MonoBehaviour
 
     public void Set_Dialogue()
     {
-        FinalDialogue_String = new string[3]
+        FinalDialogue_String = new string[5]
         {
         "아빠에게 모은 단서들을 다 보여주었다.",
-        "아빠...이게 다 뭐예요?",
-        "...그래, 너도 이제 알아야겠지, 같이 만나러 갈까?"
+        "아빠는 이제 나도 알아야 한다며 나를 엄마가 있는 절로 데려갔다.",
+        "5년전, 내가 엄마와 아빠의 싸움을 본 날, 엄마는 아빠에게 이제 한 달도 남지 않았다고 말했다.",
+        "병과 싸우며 아파하는 모습을 어린 나에게 보이고 싶지 않았던 엄마는 집을 떠났다.",
+        "아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ"
         };
     }
 }
