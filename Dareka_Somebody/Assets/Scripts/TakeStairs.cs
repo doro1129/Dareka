0927dd4aade0f0f10a69ebff0a89924901091d2a
doro1129 <doro1129@naver.com>
2022-12-08 23:26:34 +0900

    Restore to @OhJunHyun Modified credits

--- Dareka_Somebody/Assets/Scripts/TakeStairs.cs
+++ Dareka_Somebody/Assets/Scripts/TakeStairs.cs
@@ -11,12 +11,18 @@ public class TakeStairs : MonoBehaviour
 {
     // Move Player to Taget's position
     public Transform Target;
+    private AudioSource StairSound; // When Player has collision with sphere which is made for stair, it plays.
 
+    private void Start()
+    {
+        StairSound = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name == "Player")
         {
+            StairSound.Play();
             col.transform.position = Target.position;
         }
     }
