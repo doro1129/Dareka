7478758a702e2c5b50affe1ffc9245457039ab3a
Junhyun <thesimpson33@gmail.com>
2022-12-08 21:00:12 +0900

    Delete gym asset

--- Dareka_Somebody/Assets/Nimikko_JapaneseSchool_Gym/Assets/Textures/script.sh
+++
@@ -1,8 +0,0 @@
- for i in *_Masks.png; do 
-    echo "$i"
-    magick "$i" -write MPR:orig -channel B -separate -write newR.png    \
-    \( MPR:orig -channel R -separate                    -write newG.png \) \
-    \( MPR:orig -channel R -separate -threshold 100%    -write newB.png \) \
-    \( MPR:orig -channel G -separate -negate            -write newA.png \) \
-    -combine PNG32:"$i"
- done
