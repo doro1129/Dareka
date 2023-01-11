49d8ee65814c09f582e698009f02339c2f2d78b8
JunHyun <thesimpson33@gmail.com>
2023-01-12 00:26:54 +0900

    Add quit button in select

--- Dareka_Somebody/Assets/Scripts/SettingMenu.cs
+++ Dareka_Somebody/Assets/Scripts/SettingMenu.cs
@@ -9,13 +9,26 @@ public class SettingMenu : MonoBehaviour
     public AudioMixer mainMixer;
     public Slider sensitivitySlider;
 
+    private float mainVolume;
+
     public void SetVolume(float volume)
     {
         mainMixer.SetFloat("master", volume);
+        mainVolume = volume;
     }
 
     public void SetMouseSensitivity()
     {
         GetComponent<PlayerCamera>().ChangeMouseSensitivity(sensitivitySlider.value, sensitivitySlider.value);
+    }
+
+    /*public void Awake()
+    {
+        DontDestroyOnLoad(mainVolume);
+    }*/
+
+    public void Update()
+    {
+        Debug.Log(mainVolume);
     }
 }
